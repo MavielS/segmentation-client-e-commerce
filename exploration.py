07ae46ae6d75decca95cf56634210876e24c3909
MavielS <sebastien.maviel@gmail.com>
2021-08-13 01:02:56 +0200

    ...

--- exploration.py
+++ exploration.py
@@ -15,7 +15,7 @@ import plotly.graph_objects as go
 
 ############################# Affichage ############################
 
-def analyse_forme(df,thresh_na=60, all=False):
+def analyse_forme(df,thresh_na=60, figsize=(16,8), all=False):
     
     statList = {'Taille':[df.size],'Nb lignes':[df.shape[0]],
                 'Nb colonnes':[df.shape[1]],
@@ -27,7 +27,7 @@ def analyse_forme(df,thresh_na=60, all=False):
     if(all):
         # print(df.head(10))
     
-        plt.figure(figsize=(16,8))
+        plt.figure(figsize=figsize)
 
         ax1 = plt.subplot(1,2,1)
         vc = df.dtypes.value_counts()
@@ -41,7 +41,7 @@ def analyse_forme(df,thresh_na=60, all=False):
 
         squarify.plot(ax = ax1, sizes=list(vc.values), label=pd.Series(vc.keys()).to_string(index=False).replace(" ", "").splitlines(), alpha=.8, value=round(vc/vc.sum(),2), pad=0.05, color=colors,  text_kwargs={'fontsize': 22, 'fontfamily' : 'sans-serif'})
         plt.axis('off')
-        ax1.set_title('Répartition valeurs quantitatives/qualitatives',fontsize=20,weight='bold')
+        ax1.set_title('Répartition des types des variables',fontsize=20,weight='bold')
 
         ax2 = plt.subplot(1,2,2)
         perc = (df.isnull().sum()/df.shape[0])*100
@@ -64,7 +64,7 @@ def analyse_forme(df,thresh_na=60, all=False):
         plt.show()
         
     
-def mesure_forme_col(data: pd.Series, bins=None, title='Distribution et boxplot', figsize=(12,8), plotly=True):  
+def mesure_forme_col(data: pd.Series, bins=None, title='Distribution et boxplot', figsize=(12,8), plotly=True, return_fig=False):  
     '''
     Nous affiche un bel histogramme (et son boxplot associé) de la colonne data.
     '''
@@ -95,7 +95,7 @@ def mesure_forme_col(data: pd.Series, bins=None, title='Distribution et boxplot'
 
         fig.add_annotation(
             arg=  go.layout.Annotation(
-                        text=f'Skewness: {dataSkew}<br>Kurtosis: {dataKurt}<br>Moyenne: {dataMean}<br>Ecart-type: {dataStd}<br>Médiane {dataMedian}<br>Q1: {q1}<br>Q3: {q3} <br>Max: {data.max()} <br>Min {data.min()}',
+                        text=f'Skewness: {dataSkew}<br>Kurtosis: {dataKurt}<br>Moyenne: {dataMean}<br>Ecart-type: {dataStd}<br>Médiane {dataMedian}<br>Q1: {q1}<br>Q3: {q3} <br>Max: {data.max()} <br>Min: {data.min()}',
                         align='left',
                         showarrow=False,
                         xref='paper',
@@ -118,10 +118,11 @@ def mesure_forme_col(data: pd.Series, bins=None, title='Distribution et boxplot'
         )
 
 
-        fig.update_layout(height=600, width=1000, title = dict(text=title) ) 
+        fig.update_layout(height=600, width=1000, title = dict(text=title), showlegend=False ) 
 
         fig.show()
-#         return fig
+        if return_fig == True:
+            return fig
     else:
     
         fig, (ax_hist, ax_box) = plt.subplots(2, sharex=True, gridspec_kw= {"height_ratios": (1, 0.2)}, figsize=figsize)
@@ -149,11 +150,11 @@ def mesure_forme_col(data: pd.Series, bins=None, title='Distribution et boxplot'
         plt.xticks(fontsize=17)
         plt.yticks(fontsize=17)    
 
-        plt.show()
+        plt.show() 
     
 def squarify_value_counts(data: pd.Series, nb_square: int=8, title: str='Répartition valeurs quantitatives/qualitatives'):
     # TODO: Faire en sorte qu'on lui passe une figure pour ajouter un ax a cette figure
-    plt.subplots(figsize=(16,16))
+    plt.subplots(figsize=(10,10))
     
     # Sélectionne les nb_square catégories les + présentes 
     vc_tmp = data.value_counts()/data.shape[0]
@@ -179,8 +180,134 @@ def squarify_value_counts(data: pd.Series, nb_square: int=8, title: str='Répart
 
     squarify.plot(sizes=list(vc.values), label=pd.Series(vc.keys()).to_string(index=False).replace(" ", "").splitlines(), alpha=.8, value=round(vc/vc.sum(),2), pad=0.05, color=colors,  text_kwargs={'fontsize': 22, 'fontfamily' : 'sans-serif'})
     plt.axis('off')
-    plt.title(title,fontsize=20,weight='bold')
+    plt.title(title,fontsize=20, weight='bold')
 
+def heatmap(x, y, **kwargs):
+    if 'color' in kwargs:
+        color = kwargs['color']
+    else:
+        color = [1]*len(x)
+
+    if 'palette' in kwargs:
+        palette = kwargs['palette']
+        n_colors = len(palette)
+    else:
+        n_colors = 256 # Use 256 colors for the diverging color palette
+        palette = sns.color_palette("Blues", n_colors) 
+
+    if 'color_range' in kwargs:
+        color_min, color_max = kwargs['color_range']
+    else:
+        color_min, color_max = min(color), max(color) # Range of values that will be mapped to the palette, i.e. min and max possible correlation
+
+    def value_to_color(val):
+        if color_min == color_max:
+            return palette[-1]
+        else:
+            val_position = float((val - color_min)) / (color_max - color_min) # position of value in the input range, relative to the length of the input range
+            val_position = min(max(val_position, 0), 1) # bound the position betwen 0 and 1
+            ind = int(val_position * (n_colors - 1)) # target index in the color palette
+            return palette[ind]
+
+    if 'size' in kwargs:
+        size = kwargs['size']
+    else:
+        size = [1]*len(x)
+
+    if 'size_range' in kwargs:
+        size_min, size_max = kwargs['size_range'][0], kwargs['size_range'][1]
+    else:
+        size_min, size_max = min(size), max(size)
+
+    size_scale = kwargs.get('size_scale', 500)
+
+    def value_to_size(val):
+        if size_min == size_max:
+            return 1 * size_scale
+        else:
+            val_position = (val - size_min) * 0.99 / (size_max - size_min) + 0.01 # position of value in the input range, relative to the length of the input range
+            val_position = min(max(val_position, 0), 1) # bound the position betwen 0 and 1
+            return val_position * size_scale
+    if 'x_order' in kwargs: 
+        x_names = [t for t in kwargs['x_order']]
+    else:
+        x_names = [t for t in sorted(set([v for v in x]))]
+    x_to_num = {p[1]:p[0] for p in enumerate(x_names)}
+
+    if 'y_order' in kwargs: 
+        y_names = [t for t in kwargs['y_order']]
+    else:
+        y_names = [t for t in sorted(set([v for v in y]))]
+    y_to_num = {p[1]:p[0] for p in enumerate(y_names)}
+
+    plot_grid = plt.GridSpec(1, 15, hspace=0.2, wspace=0.1) # Setup a 1x10 grid
+    ax = plt.subplot(plot_grid[:,:-1]) # Use the left 14/15ths of the grid for the main plot
+
+    marker = kwargs.get('marker', 's')
+
+    kwargs_pass_on = {k:v for k,v in kwargs.items() if k not in [
+         'color', 'palette', 'color_range', 'size', 'size_range', 'size_scale', 'marker', 'x_order', 'y_order'
+    ]}
+
+    ax.scatter(
+        x=[x_to_num[v] for v in x],
+        y=[y_to_num[v] for v in y],
+        marker=marker,
+        s=[value_to_size(v) for v in size], 
+        c=[value_to_color(v) for v in color],
+        **kwargs_pass_on
+    )
+    ax.set_xticks([v for k,v in x_to_num.items()])
+    ax.set_xticklabels([k for k in x_to_num], rotation=45, horizontalalignment='right')
+    ax.set_yticks([v for k,v in y_to_num.items()])
+    ax.set_yticklabels([k for k in y_to_num])
+
+    ax.grid(False, 'major')
+    ax.grid(True, 'minor')
+    ax.set_xticks([t + 0.5 for t in ax.get_xticks()], minor=True)
+    ax.set_yticks([t + 0.5 for t in ax.get_yticks()], minor=True)
+
+    ax.set_xlim([-0.5, max([v for v in x_to_num.values()]) + 0.5])
+    ax.set_ylim([-0.5, max([v for v in y_to_num.values()]) + 0.5])
+    ax.set_facecolor('#F1F1F1')
+
+    # Add color legend on the right side of the plot
+    if color_min < color_max:
+        ax = plt.subplot(plot_grid[:,-1]) # Use the rightmost column of the plot
+
+        col_x = [0]*len(palette) # Fixed x coordinate for the bars
+        bar_y=np.linspace(color_min, color_max, n_colors) # y coordinates for each of the n_colors bars
+
+        bar_height = bar_y[1] - bar_y[0]
+        ax.barh(
+            y=bar_y,
+            width=[5]*len(palette), # Make bars 5 units wide
+            left=col_x, # Make bars start at 0
+            height=bar_height,
+            color=palette,
+            linewidth=0
+        )
+        ax.set_xlim(1, 2) # Bars are going from 0 to 5, so lets crop the plot somewhere in the middle
+        ax.grid(False) # Hide grid
+        ax.set_facecolor('white') # Make background white
+        ax.set_xticks([]) # Remove horizontal ticks
+        ax.set_yticks(np.linspace(min(bar_y), max(bar_y), 3)) # Show vertical ticks for min, middle and max
+        ax.yaxis.tick_right() # Show vertical ticks on the right 
+
+
+def corrplot(data, size_scale=500, marker='s'):
+    corr = pd.melt(data.reset_index(), id_vars='index')
+    corr.columns = ['x', 'y', 'value']
+    heatmap(
+        corr['x'], corr['y'],
+        color=corr['value'], color_range=[-1, 1],
+        palette=sns.diverging_palette(20, 220, n=256),
+        size=corr['value'].abs(), size_range=[0,1],
+        marker=marker,
+        x_order=data.columns,
+        y_order=data.columns[::-1],
+        size_scale=size_scale
+    )
     
 #####################################################################
         
